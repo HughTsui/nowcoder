a9c5fea57b7a18e106150444061a36d36eac49a5
HughTsui <384165496@qq.com>
2017-05-02 03:48:41 +0800

    Update ScrapyManage.py

--- ScrapyManage.py
+++ ScrapyManage.py
@@ -44,7 +44,7 @@ class ScrapyManager(object):
 		print('start...')
 		if self.process:
 			self.stop()
-		self.process = subprocess.Popen((self.RUN_SPIDER),cwd='nowcoder-master')
+		self.process = subprocess.Popen((self.RUN_SPIDER),cwd='now_coder')
 		
 	def stop(self):
 		print('stop...')
@@ -54,10 +54,6 @@ class ScrapyManager(object):
 			
 	def deploy(self):
 		print('start to deploy...')
-		with open('nowcoder-master.zip','wb') as f:
-			f.write(requests.get('https://github.com/HughTsui/nowcoder/archive/master.zip'))
-		if self.platform.startswith('lin'):
-			os.system('unzip -o nowcoder-master.zip')
 		self.stop()
 		self.start()	
 		
