1f5fbf4277b39f96c60745ebe408849dbb75c6be
HughTsui <384165496@qq.com>
2017-05-04 16:22:55 +0800

    Update settings.py

--- now_coder/now_coder/settings.py
+++ now_coder/now_coder/settings.py
@@ -27,7 +27,7 @@ SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.SpiderPriorityQueue' #爬虫调度�
 
 
 
-MONGODB_HOST = '127.0.0.1'
+MONGODB_HOST = '120.25.78.80'
 MONGODB_PORT = 27017
 MONGODB_DBNAME = 'NowcoderPromblemList'
 MONGODB_DOCNAME = 'PList_dis'
